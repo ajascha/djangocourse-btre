3f27c84a5dd21022373fb167228aa77846820d13
Arne Wolfewicz <arne.wolfewicz@gmail.com>
2019-08-14 11:28:38 +0200

    login & register stuff

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    """
+    Test if registration works: put in sth in login box & check terminal output
+    print('SUBMITTED REG')
+    return redirect('register')
+    """
+    if request.method == 'POST':
+        # Register user
+        return
+    else:
+        return render(request, 'accounts/register.html')
+
+
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        # Login
+        return
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     return redirect('index')
