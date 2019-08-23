c7c07b321b87897999a6821dca5c538cfc0ff316
Arne Wolfewicz <arne.wolfewicz@gmail.com>
2019-08-23 09:20:51 +0200

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'arne.wolfewicz@gmail.com'
 EMAIL_HOST_PASSWORD = 'vpklilwzzrokvbie'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
